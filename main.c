b043dfbc75addeb888a3fce537705e702e5bf15c
italoepifanio <italoepifaniols@gmail.com>
2017-11-01 22:34:30 -0300

    Adicao de comentarios, leitura do arquivo sendo realizada

--- main.c
+++ main.c
@@ -7,21 +7,11 @@ int main(){
 	int i, j;
 	FILE *file = readFile("Normal.ppm");
 	Image *image = getImage(file);
-	
+	int width = image->width;
+	int height = image->height;
 	printf("Largura: %i\n", image->width);
 	printf("Altura: %i\n", image->height);	
 
-	//Tentando inicializar matriz de pixels
-
-	Pixel matrizPixel[image->width][image->height];
-
-	for(i = 0; i < image->width; i++){
-		for(j = 0; j < image->height; j++){
-			fscanf(file, "%i", &matrizPixel[i][j].r);
-			fscanf(file, "%i", &matrizPixel[i][j].g);
-			fscanf(file, "%i", &matrizPixel[i][j].b);
-		}
-	}
-
+	printf("%i", image->pixels[0][0].r);
 	return 0;
 }
--- ppmTreatment.h
+++ ppmTreatment.h
@@ -2,25 +2,57 @@ FILE * readFile(char filename[]){
 	FILE *file;
 
 	file = fopen(filename, "r");
+
+	if (! file){
+		printf("Ocorreu algum erro durante a leitura do arquivo");
+		exit(1);
+	}
+
 	//Adicionar testes no arquivo aqui
 	return file;
 }
 
 //Read and return image
 Image * getImage(FILE *file){
-	int pixel;
-	char header[16];
-	Image *img = (Image *)malloc(sizeof(Image));
+	char header[2];
+	int i, j, jumpLine;
+	
+	//fgets lê a primeira linha do arquivo e atribui ao tipo
+	fscanf(file, "%s", header);
 
-	//fgets lê a primeira linha do arquivo
-	fgets(header, sizeof(header), file);
+	//Checa tipo da imagem P3
+	if(header[0] != 'P' && header[1]!= '3'){
+		printf("Tipo da imagem não suportado");
+		exit(1);
+	}
+	
+	Image *img = malloc(sizeof(Image)*1);
+
+	if(! img){
+		printf("A imagem não conseguiu ser alocada na memória");
+		exit(1);
+	}
 	
 	//Lê uma sequência de caracteres como unsigned char
 	//E atribui os valores da linha ao tipo Image *img
 	fscanf(file, "%d %d", &img->width, &img->height);
 
-	//Pula a linha para ir aos pixels
-	while (fgetc(file) != '\n');	
+	//Gambiarra para pular linha do tamanho
+	fscanf(file, "%d", &jumpLine);
+
+	img->pixels = (Pixel **)malloc(sizeof(Pixel*) * img->width);
+
+	for(i = 0; i < img->width; i++){
+		img->pixels[i] = (Pixel*)malloc(img->height * sizeof(Pixel));
+	}
+
+	for(i = 0; i < img->width; i++){
+		for(j = 0; j < img->height; j++){
+			fscanf(file, "%i%i%i", &img->pixels[i][j].r, &img->pixels[i][j].g, &img->pixels[i][j].b);		
+		}
+	}
+	
+	fclose(file);
 
 	return img;
 }
