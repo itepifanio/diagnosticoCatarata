45123cab096ba8b94ff05924f9ec71c21ed5c3f6
italoepifanio <italoepifaniols@gmail.com>
2017-11-04 10:28:17 -0300

    Removendo linhas de comentarios desnecessarias e criando mini docs

--- primitive.h
+++ primitive.h
@@ -1,25 +1,29 @@
+/**
+* A enum to create booleans 
+* type in c
+**/
 enum bool
 {
 	true = 1,
 	false = 0
 } bool;
 
+/**
+* A new typedef with rgb pattern
+**/
 typedef struct Pixels
 {
-	/*
-	A new typedef with rgb pattern
-	*/
 	int r, g, b;
 } Pixel;
 
+/**
+* A new typedef who read an image
+* with width, height integer,
+* an header char[54] and the 
+* Pixel typedef
+**/
 typedef struct Images
 {
-	/*
-	A new typedef who read an image
-	with width, height integer,
-	an header char[54] and the 
-	Pixel typedef
-	*/
 	int width, height;
 	char header[54];
 	Pixel **pixels;
