e863adae2c0b845282109a8fe556d3f446452520
Italo Epifânio de Lima e Silva <itepifanio@A305-05L.imd.local>
2017-11-24 08:31:17 -0300

    VAAAIII

--- main.c
+++ main.c
@@ -1,11 +1,35 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include "libs/filters.h"
+/*
+void checkArgv(char *argv[]){
+	char *nomeArquivo = strcat("images/", argv[2]);
+	FILE *file = fopen(nomeArquivo, "r");
+
+	if(! file){
+		printf("Ops, ocorreu algum erro com seus argumentos\n");
+		printf("Os nomes devem ser no formato: \n");
+		printf("./catarata -i images/Catarata.ppm\n");
+		exit(1);
+	}else{
+		printf("Inicializando image, aguarde... %s\n", argv[2]);
+	}
+}
+*/
+int main(int argc, char *argv[]){
+	char *nomeArquivo = argv[2];
+	char *batata = "batata";
+	strcat(batata, nomeArquivo);
+	
+	if((out = (char *)malloc(strlen(nomeArquivo) + strlen(batata) + 1)) != NULL)
+	{
+   		strcpy(out, nomeArquivo);
+   		strcat(out, batata);
+	}
 
 
-int main(){
-	int i, j;
-	FILE *file = readFile("images/Catarata.ppm");
+	FILE *file = readFile(strcat("images/", nomeArquivo));
 	Image *image = getImage(file);
 
 	Image *grayImage = grayScale(image);
@@ -22,7 +46,7 @@ int main(){
 	Image *binaryImage = binary(sobel);
 	saveImage("images/binary.ppm", binaryImage, 1);
 	
-	file = readFile("images/Catarata.ppm");
+	file = readFile(argv[2]);
 	image = getImage(file);
 	
 	Image *hough = houghTransform(binaryImage, image);
