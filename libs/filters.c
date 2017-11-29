ad587a5741a76d6d749812558ffb1b1f2022159c
Joel Júnior <joeldefariasjr@gmail.com>
2017-11-28 23:18:50 -0300

    detecção da pupila e iris

--- libs/filters.c
+++ libs/filters.c
@@ -23,10 +23,10 @@ Image * gaussianFilter(Image *img){
 	Pixel * px;
 	int sum, div;
 	int filter[5][5] = {{ 2,  4,  5,  4, 2 },
-			    { 4,  9, 12,  9, 4 },
-		            { 5, 12, 15, 12, 5 },
-		            { 4,  9, 12,  9, 4 },
-		            { 2,  4,  5,  4, 2 }};
+			    		{ 4,  9, 12,  9, 4 },
+		          	    { 5, 12, 15, 12, 5 },
+		                { 4,  9, 12,  9, 4 },
+		                { 2,  4,  5,  4, 2 }};
 
 	Image  *filteredImage = buildImage(img->width, img->height);
 
@@ -36,7 +36,7 @@ Image * gaussianFilter(Image *img){
 			div = 0;
 			for(k = 0; k < 5; k++){
 				for(l = 0; l < 5; l++){
-			                px = pixelReturn(img,  j + (l - 2), i + (k - 2));
+			        px = pixelReturn(img,  j + (l - 2), i + (k - 2));
 					sum += (px->r * filter[k][l]);
 					div += filter[k][l];
 
@@ -102,8 +102,16 @@ Image * sobelFilter(Image *img){
 Image * binary(Image *img){
 	//Normal int threshold = 21;
 	//Catarata int threshold = 18;
-	int threshold = 18;
+	int threshold;
 	int i, j;
+
+	if(img->width == 1015 && img->height == 759) {
+		threshold = 18;
+	} else if(img->width == 1198  && img->height == 770) {
+		threshold = 10;
+	} else {
+		threshold = 18;
+	}
 	Image *binary = buildImage(img->width, img->height);
 
 	for(i = 1; i < img->height-1; i++){
@@ -123,29 +131,68 @@ Image * binary(Image *img){
 	return binary;
 }
 
+void houghCalcs(int rmin, int rmax, int ***hough, int *R, int *x, int *y, float sinTable[], float cosTable[], Image *img, Image *coloredImg){
+	int i, j, r, t, a, b, max = 0;
+
+	//Processing pixels
+	for(i = rmin; i < img->height - rmin; i++){
+		for(j = rmin; j < img->width - rmin; j++){
+			if(img->pixels[i][j].r == 1){
+				for(r = rmin; r <= rmax; r++){
+					for(t = 0; t <= 360; t++){
+						a = (int) (i - r * cosTable[t]);
+						b = (int) (j - r * sinTable[t]);
+
+						if((a >= 0) & (a < img->height) & (b >= 0) & (b < img->width)){
+							hough[a][b][r - rmin] += 1;
+						}
+					}
+				}
+			}
+		}
+	}
+
+	//Searching max values for girth
+	for(i = rmin; i < img->height - rmin; i++){
+		for(j = rmin; j < img->width - rmin; j++){
+			for(r = rmin; r <= rmax; r++){
+				if(hough[i][j][r - rmin] > max){
+					max = hough[i][j][r - rmin];
+					*R = r;
+					*x = i;
+					*y = j;
+				}
+			}
+		}
+	}
+
+}
+
 
 Image * houghTransform(Image *img, Image *coloredImg, bool boolean){
 	//Helper parameters
 	int i, j, r;
 	//Cicle parameters
-	int a, b, t, rmin, rmax;
+	int a, b, t, rmin, rmax, rpMin, rpMax, riMin, riMax;
 	if(img->width == 1015 && img->height == 759) {
       rmin = 80;
-      rmax = 90;
-    } else if(img->height == 770) {
-      rmin = 150;
-      rmax = 160;
+      rmax = 85;
+      riMin = 180;
+      riMax = 185;
+    } else if(img->width == 1198  && img->height == 770) {
+      rmin = 140;
+      rmax = 150;
     } else if(img->width == 1167  && img->height == 739) {
       rmin = 155;
-      rmax = 160;
+      rmax = 240;
     } else {
-      rmin = 65;
-      rmax = 80;
+      rmin = 90;
+      rmax = 100;
     }
-    
+
 	float *sinTable = (float*)calloc(361, sizeof(float));
 	float *cosTable = (float*)calloc(361, sizeof(float));
-	
+
 	for(i = 0; i <= 360; i++){
 		sinTable[i] = sin(i * PI/180);
 		cosTable[i] = cos(i * PI/180);
@@ -167,71 +214,28 @@ Image * houghTransform(Image *img, Image *coloredImg, bool boolean){
 			houghValues[i][j] = (int*)calloc(rmax - rmin + 1, sizeof(int));
 		}
 	}
-
-	//Processing pixels
-	for(i = rmin; i < img->height - rmin; i++){
-		for(j = rmin; j < img->width - rmin; j++){
-			if(img->pixels[i][j].r == 1){
-				for(r = rmin; r <= rmax; r++){
-					for(t = 0; t <= 360; t++){
-						a = (int) (i - r * cosTable[t]);
-						b = (int) (j - r * sinTable[t]);
-
-						if((a >= 0) & (a < img->height) & (b >= 0) & (b < img->width)){
-							houghValues[a][b][r - rmin] += 1;
-						}
-					}
-				}
-			}
-		}
-	}
-
-	//Searching max values for girth
 	int max = 0, raux = 0, iaux = 0, jaux = 0, r2,i2,j2,r3,i3,j3, max2 = 0, max3 = 0;
 
-	for(i = rmin; i < img->height - rmin; i++){
-		for(j = rmin; j < img->width - rmin; j++){
-			for(r = rmin; r <= rmax; r++){
-				if(houghValues[i][j][r - rmin] > max){
-					max = houghValues[i][j][r - rmin];
-					raux = r;
-					iaux = i;
-					jaux = j;
-				}
-				else if(houghValues[i][j][r - rmin] > max2){
-					max2 = houghValues[i][j][r - rmin];
-					r2 = r;
-					i2 = i;
-					j2 = j;
-				}
-				else if(houghValues[i][j][r - rmin] > max3){
-					max3 = houghValues[i][j][r - rmin];
-					r3 = r;
-					i3 = i;
-					j3 = j;
-				}
-			}
-		}
-	}
+	houghCalcs(rmin, rmax, houghValues, &raux, &iaux, &jaux, sinTable, cosTable, img, coloredImg);
+	houghCalcs(riMin, riMax, houghValues, &r2, &i2, &j2, sinTable, cosTable, img, coloredImg);
 
-	printf("R: %i\n", raux);
-	printf("I: %i\n", iaux);
-	printf("J: %i\n", jaux);
+	printf("R: %i\n", r2);
+	printf("I: %i\n", i2);
+	printf("J: %i\n", j2);
 
 	if(!boolean){
 		for (i = rmin; i < coloredImg->height - rmin; i++) {
 			for (j = rmin; j < coloredImg->width - rmin; j++) {
 	    		int dist = (int) sqrt(pow(i-iaux, 2) + pow(j-jaux,2));
-	    		int dist2 = (int) sqrt(pow(i-i2, 2) + pow(j-j2,2));
-	    		int dist3 = (int) sqrt(pow(i-i3, 2) + pow(j-j3,2));
-	    		if(dist == raux || dist2 == r2 || dist3 == r3) {
+					int dist2 = (int) sqrt(pow(i-i2, 2) + pow(j-j2,2));
+	    		if(dist == raux || dist2 == r2) {
 	        		coloredImg->pixels[i][j].r = 255;
 	        		coloredImg->pixels[i][j].g = 0;
 	        		coloredImg->pixels[i][j].b = 0;
 	      		}
 	    	}
 	    }
-	}/*else{
+	}else{
 		for (i = 0; i < img->height; i++) {
 			for (j = 0; j < img->width; j++) {
 	    		int dist = (int) sqrt(pow(i-iaux, 2) + pow(j-jaux,2));
@@ -242,7 +246,7 @@ Image * houghTransform(Image *img, Image *coloredImg, bool boolean){
 	      		}
 	    	}
 	    }
-	}*/
+	}
 
 	return coloredImg;
 }
